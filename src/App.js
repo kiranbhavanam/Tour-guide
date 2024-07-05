00580bc718469b40a4e7de1b386adcae3e703dbe
kiran22022002 <54470122+kiran22022002@users.noreply.github.com>
2024-07-05 23:34:50 +0530

    Added Sort functionality

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function removeHandler(id){
   return (
     <div className="App flex flex-col justify-center items-center">
       <h3 className='text-2xl text-violet-700 px-8 py-2  mt-[50px] border-dashed border-4 border-violet-800 rounded-md'>Trip Planner</h3>
-      <Tours tours={tours} removeHandler={removeHandler}></Tours>
+      <Tours tours={tours} setTours={setTours} removeHandler={removeHandler}></Tours>
     </div>
   );
 }
--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -1,16 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Cards from './Cards'
-const Tours = ({tours,removeHandler}) => {
-    
+const Tours = ({tours,setTours,removeHandler}) => {
+    const [sort,setSort]=useState("");
+    function sortHandler(e){
+      setSort(e.target.value);
+      
+    }
+    useEffect(()=>{
+      console.log("sort by :",sort)
+      const sortedTours=[...tours]
+      if(sort==="cost"){
+
+        sortedTours.sort((a,b)=>a.cost-b.cost)
+      }
+      if(sort==="city"){
+        sortedTours.sort((a,b)=>a.city.localeCompare(b.city))
+
+      }
+
+      setTours(sortedTours)
+      console.log("sorted tours,",sortedTours)
+    },[sort])
+
   return (
-        <div  className='max-w-[1000px] flex items-center justify-center flex-wrap gap-5 mx-auto my-[70px]'>
+        <div  className='max-w-[1000px] flex items-center justify-center flex-wrap gap-5 mx-auto my-[70px] relative'>
+          <div className='absolute -top-[100px] right-0 border px-2 rounded-sm'>
+            <select name="sort" id="" value={sort} onChange={sortHandler }>
+              {/* <option value="sort by">sort by</option> */}
+              <option value="cost">cost</option>
+              <option value="city">city</option>
+            </select>
+          </div>
           {
             tours.map(tour=>{
               return(
-                  <Cards  {...tour} removeHandler={removeHandler}></Cards>
+                  <Cards key={tour.id} {...tour} removeHandler={removeHandler}></Cards>
               )
             })
           }
+          
         </div>
   )
 }
